ea3a0950c39db92d93d4c90cfac29bb55ba5c005
PG <pg.lemasle@gmail.com>
2019-03-28 12:18:30 +0100

    Added basic documentation Added package library to global.R for shiny app

--- R/launch_multi.R
+++ R/launch_multi.R
@@ -1,3 +1,9 @@
+#' Function is to launch the GUI of the package, based on Shiny.
+#'
+#' `launch_multi()` If used on Rstudio the function will launch an interface to run
+#' the analysis graphically.
+#' @author Pierre-Gilles Lemasle <pg.lemasle@gmail.com>
+#' @export
 #' @export
 launch_multi <- function() {
   appDir <- system.file("multi", package = "Multi")
